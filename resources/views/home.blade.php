68fd1239053792609160df5d5d90333af85bdb1b
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-16 07:10:35 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        @include('layouts.main')
+        @extends('layouts.main')
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
