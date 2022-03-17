b16eeba2f46b7f638c0d59862a41d675e8f06589
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-17 07:43:10 -0300

    Update create.blade.php

--- resources/views/Evento/create.blade.php
+++ resources/views/Evento/create.blade.php
@@ -30,6 +30,32 @@
 					<li><a href="{{route('manutencao.index')}}">Manutenção</a> </li>
 					<li><a href="{{route('previsaoentregar.index')}}">Previsão de entregar de equipamentos </a> </li>
 				</ul>
+                
+                <div>
+                   <form action="{{route('evento.store')}}" method="post">
+                     @csrf
+                <div class="row g-3">
+                    <div class="row"><h4>cadastro de eventos</h4></div>
+                    <div class="col">
+                        <label for="nome">nome</label>
+                        <input type="text" class="form-control" name="nome" >
+                         <label>data de inicio</label>
+                         <input type="date" class="form-control" name="data_inicio">
+                        <label>data de fim</label>
+                         <input type="date" class="form-control" name="data_fim">
+                          
+                 
+                        <label>hora de inicio</label>
+                         <input type="time" class="form-control" name="hora_inicio">
+                          <label>hora de fim</label>
+                         <input type="time" class="form-control" name="hora_terminio"><br> 
+                         <input type="submit" class="btn btn-primary" value="cadastrar">      
+                </div>            
+            </form>             
+                
+                
+                
+                </div>
 			</div>
 	</div>
 
@@ -64,26 +90,7 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                             
-                             <form action="{{route('evento.store')}}" method="post">
-                     @csrf
-                <div class="row g-3">
-                    <div class="row"><h4>cadastro de eventos</h4></div>
-                    <div class="col">
-                        <label for="nome">nome</label>
-                        <input type="text" class="form-control" name="nome" >
-                         <label>data de inicio</label>
-                         <input type="date" class="form-control" name="data_inicio">
-                        <label>data de fim</label>
-                         <input type="date" class="form-control" name="data_fim">
                           
-                 
-                        <label>hora de inicio</label>
-                         <input type="time" class="form-control" name="hora_inicio">
-                          <label>hora de fim</label>
-                         <input type="time" class="form-control" name="hora_terminio"><br> 
-                         <input type="submit" class="btn btn-primary" value="cadastrar">      
-                </div>            
-            </form>             
                         </div>
                     @endif
                    
