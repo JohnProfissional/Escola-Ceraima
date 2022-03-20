71ccd95134c207d1585fb601d104246f4286581a
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-20 08:22:15 -0300

    Update PatrimonioController.php

--- app/Http/Controllers/PatrimonioController.php
+++ app/Http/Controllers/PatrimonioController.php
@@ -51,6 +51,6 @@ class PatrimonioController extends Controller{
             $Patrimonio->localizacao=$request->localizacao;
             $Patrimonio->save();
         
-         redirect()->route('patrimonio.index');
+         redirect()->route('patrimonio.index',['id' => $Patrimonio->id]);
     }
 }
