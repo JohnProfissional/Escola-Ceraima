4e2de0670589c41752de1f3096d52b8da0441e32
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-21 22:36:57 -0300

    Update create.blade.php

--- resources/views/patrimonio/create.blade.php
+++ resources/views/patrimonio/create.blade.php
@@ -166,7 +166,7 @@
       
  include('layout.app')
  
-      
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                       
             <h2>Cadastre-se os dados do patrimonio</h2><br>
            
