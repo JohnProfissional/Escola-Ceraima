0ed0476fafbdd5072e0d9d0fa5aa68d05e83c765
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-21 23:17:14 -0300

    Update create.blade.php

--- resources/views/patrimonio/create.blade.php
+++ resources/views/patrimonio/create.blade.php
@@ -254,6 +254,35 @@
             </form>
                       
         </x-slot>
+
+
+        <div class="row g-3">
+    <h4 align="justify">Cadastro de entrada</h4>
+
+    <div class="col">
+        <form action="{{route('patrimonio.store')}}" method="post">
+            @csrf
+            <label for="nome">valor total da nota</label><br>
+            <input type="text" class="form-control" name="valortotaldanota" id="valortotaldanota">
+
+            <label for="dataentrada">data da entrada</label><br>
+            <input type="date" class="form-control" name="dataentrada" id="dataentrada">
+
+            <label for="fornecedor_id">fornecedor</label><br>
+            {{-- pega a variavel passada pelo metodo create do controller --}}
+
+            <select name="fornecedor_id" class="form-control" name="fornecedor_id">
+                @foreach($Fornecedor as $fornecedor)
+                    <option  class="form-control" name="fornecedor_id">
+                        {{ $fornecedor->razaosocial }}
+                    </option>
+                @endforeach
+            </select><br>
+
+            <input type="submit" class="btn btn-primary" value="cadastrar">
+
+    </div>
+
                     
 
 
