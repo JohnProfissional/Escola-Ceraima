aa86aa503134364507c237d38b6d7e88a7f5a31f
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-22 15:52:23 -0300

    Update index.blade.php

--- resources/views/patrimonio/index.blade.php
+++ resources/views/patrimonio/index.blade.php
@@ -1,35 +1,16 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/styles.css'); }}">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=2.0">
-	<title>Document</title>
-</head>
-<body>
+@extends('layouts.main')
 @extends('layouts.app')
 
 
-	<div id="header">
-			<h1>@yield('titulo')</h1>
-			<div id="nav">
-				<ul id="menu-h">
-                    <li><a href="{{route('home')}}">Home</a> </li>
-					<li><a href="{{route('evento.index')}}">Eventos</a> </li>
-					<li><a href="{{route('usuario.index')}}">Usuario</a> </li>
-					<li><a href="{{route('reservas.index')}}">Reserva</a> </li>
-					<li><a href="{{route('sala.index')}}">Sala</a> </li>
-					<li><a href="{{route('patrimonio.index')}}">Patrimonio</a> </li>
-					<li><a href="{{route('testes.index')}}">Testes</a> </li>
-					<li><a href="{{route('manutencao.index')}}">Manutenção</a> </li>
-					<li><a href="{{route('previsaoentregar.index')}}">Previsão de entregar de equipamentos </a> </li>
-				</ul>
-             </div>
-     </div>
-                <div class="conteudo">
+
+
+
+
+@section('conteudo')
+
+    
+    
     @section('titulo','lista de patrimonio')                
     <table class="table table-sm">
           @foreach ($Patrimonio as $patrimonio)
@@ -96,9 +77,8 @@
        
 <div>
    
-     
-   
-                    
+    
+         
                    <footer>
 			<br>
 			<br><br>
@@ -107,19 +87,9 @@
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
             </div>
 </footer>
-</div>
+   
+@endsection('conteudo')
 
-
-
-
-	
-
-
-
-
-
-</body>
-</html>
 
 
 
