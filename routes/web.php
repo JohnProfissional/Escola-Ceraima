3b1528a2d2ea3da0fdcb03dff692205ba78b3f79
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-22 15:32:08 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -134,8 +134,7 @@ Route::post('/storepatrimonio',[PatrimonioController::class,'store'])->middlewar
 
 Route::post('/patrimonio/edit/{id}',[PatrimonioController::class,'edit'])->middleware(['auth'])->name('patrimonio.edit');
 Route::put('/patrimonio/edit/{id}',[PatrimonioController::class,'edit'])->middleware(['auth'])->name('patrimonio.edit');
-Route::delete('/patrimonio/update',[PatrimonioController::class, 'update'])->middleware(['auth'])->name('patrimonio.update');
-
+Route::put('/patrimonio/update/{id}',[PatrimonioController::class, 'update'])->name('patrimonio.update');
 Route::delete('/patrimonio/{id}',[PatrimonioController::class, 'destroy'])->middleware(['auth'])->name('patrimonio.delete');
 
 
