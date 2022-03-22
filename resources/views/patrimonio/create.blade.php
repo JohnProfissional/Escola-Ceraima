a7a4f054d2376bcdaa153862b2aa9d13a6f1730f
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-21 23:26:16 -0300

    Update create.blade.php

--- resources/views/patrimonio/create.blade.php
+++ resources/views/patrimonio/create.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
     <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
     <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
-    <title>Pacote-Turismo</title>
+    <title>Escola Colônia Agricola</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,7 +26,7 @@
   <body class="app sidebar-mini">
   
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.html">Pacote-Turismo</a>
+    <header class="app-header"><a class="app-header__logo" href="index.html">Escola Colônia Agricola</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
@@ -93,10 +93,9 @@
     <aside class="app-sidebar">
       
       <ul class="app-menu">
-        <li><a class="app-menu__item" href="dashboard.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">Instrutor</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             
-                <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">Turista</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label"><a href="{{route('evento.index')}}">Eventos</a>
+        </span><i class="treeview-indicator fa fa-angle-right"></i></a>
         
           <ul class="treeview-menu">
             <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
@@ -105,15 +104,43 @@
             <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
           </ul>
         </li>
-        <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">Passeio</span></a></li>
+        <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label"><a href="{{route('usuario.index')}}">Usuario</a></span></a></li>
        
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Hotel</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label"><a href="{{route('reservas.index')}}">Reserva</a></span><i class="treeview-indicator fa fa-angle-right"></i></a>
+
+
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">
+          <a href="{{route('sala.index')}}">Sala</a>
+        </span><i class="treeview-indicator fa fa-angle-right"></i></a>
+
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">
+          <a href="{{route('sala.index')}}"><a href="{{route('patrimonio.index')}}">Patrimonio</a>
+        </a>
+        </span><i class="treeview-indicator fa fa-angle-right"></i></a>
+
+
+         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">
+          <a href="{{route('testes.index')}}">Testes</a>
+        </a>
+        </span><i class="treeview-indicator fa fa-angle-right"></i></a>
+
+         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">
+          <a href="{{route('manutencao.index')}}">Manutenção</a> 
+        </a>
+        </span><i class="treeview-indicator fa fa-angle-right"></i></a>
+
+         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">
+          <a href="{{route('previsaoentregar.index')}}">Previsão de entregar de equipamentos </a> 
+        </a>
+        </span><i class="treeview-indicator fa fa-angle-right"></i></a>
+
+
           <ul class="treeview-menu">
             <li><a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic Tables</a></li>
             <li><a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data Tables</a></li>
           </ul>
         </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Notas Fiscal</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+       
           <ul class="treeview-menu">
             <li><a class="treeview-item" href="blank-page.html"><i class="icon fa fa-circle-o"></i> Blank Page</a></li>
             <li><a class="treeview-item" href="page-login.html"><i class="icon fa fa-circle-o"></i> Login Page</a></li>
@@ -127,8 +154,7 @@
         </li>
       </ul>
     </aside>
-    <main class="app-content">
-      <div class="app-title">
+  
         <div>
          
         </div>
@@ -138,9 +164,55 @@
         </ul>
       </div>
       
-    <x-app-layout>
+
+
  
-        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                      
+            <h2>Cadastre-se os dados do patrimonio</h2><br>
+           
+                    <div class="pull-right">
+                       
+                        <a class="btn btn-primary" href="">Voltar</a>
+                    </div>
+                    
+                       
+                            
+                            
+                                   
+                                        <form class="row g-3" action="{{route('patrimonio.store')}}" method="post">
+          
+                                            @csrf
+                                  
+                                          <label for="formGroupExampleInput2" class="form-label">nome</label><br>
+                                          <div class="col-md-4">
+                                           <label for="inputPassword4" class="form-label">nome</label>
+                                          <input type="text" class="form-control" name="nome" id="nome">
+                                        
+                                        
+                                           <label for="inputPassword4" class="form-label">tipo de patrimonio</label>
+                                          <input type="text" class="form-control" name="tipo_patrimonio" id="nome">
+                                  
+                                           <label for="inputPassword4" class="form-label">quantidade de patrimonio</label>
+                                          <input type="text" class="form-control" name="quantidade_patrimonio" id="nome"><br>
+                                       
+                                      
+                                           <label for="inputPassword4" class="form-label">localizacao</label>
+                                           <input type="text" class="form-control" name="localizacao" id="nome"><br>
+                                     
+                                      
+
+                                           <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                              <button type="submit" class="btn btn-primary">Adicionar</button>
+                                           </div>
+                                         </div>     
+                                            
+                               
+                        
+                        </form>
+
+
+                         <x-slot name="header">
                        
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             <h2>Cadastre-se os dados do hotel</h2><br>
@@ -182,19 +254,96 @@
             </form>
                       
         </x-slot>
+
+
+        <div class="row g-3">
+    <h4 align="justify">Cadastro de entrada</h4>
+
+    <div class="col">
+        <form action="{{route('patrimonio.store')}}" method="post">
+            @csrf
+            <label for="nome">valor total da nota</label><br>
+            <input type="text" class="form-control" name="valortotaldanota" id="valortotaldanota">
+
+            <label for="dataentrada">data da entrada</label><br>
+            <input type="date" class="form-control" name="dataentrada" id="dataentrada">
+
+            <label for="fornecedor_id">fornecedor</label><br>
+            {{-- pega a variavel passada pelo metodo create do controller --}}
+
+            
+
+            <input type="submit" class="btn btn-primary" value="cadastrar">
+
+    </div>
+
                     
-        
+
+
+
+                        <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
+
+                      
+    
+                    </div>
+          
+      
                             
       
                     
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/styles.css'); }}">
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=2.0">        
+                    <meta name="viewport" content="width=device-width, initial-scale=2.0">   
+
+
+
                             
                                      
-                      
-                </x-app-layout>
+            
                 
 
             </div>
@@ -233,8 +382,3 @@
     </script>
   </body>
 </html>
-
-         
-
-   
-   
