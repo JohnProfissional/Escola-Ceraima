a172db01252e38f19c190f52acaa2d55209b73c8
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-21 21:49:42 -0300

    Update create.blade.php

--- resources/views/patrimonio/create.blade.php
+++ resources/views/patrimonio/create.blade.php
@@ -26,7 +26,7 @@
   <body class="app sidebar-mini">
   
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.html">Pacote-Turismo</a>
+    <header class="app-header"><a class="app-header__logo" href="index.html">Escola Colônia Agricola</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
