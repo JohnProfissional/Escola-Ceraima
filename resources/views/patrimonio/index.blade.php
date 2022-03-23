1e4bed9cf4b78e97d58509171d8da46bbc26a2d0
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2022-03-22 22:44:52 -0300

    Update index.blade.php

--- resources/views/patrimonio/index.blade.php
+++ resources/views/patrimonio/index.blade.php
@@ -31,13 +31,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif   
+               
  <table class="table table-sm">
  @section('titulo','lista de patrimonio') 
  <form action="{{route('patrimonio.create')}}" method="post">
